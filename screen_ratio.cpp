23ffd027c092b393a3b6be6b947e50647a694a5f
Roker2 <dimaminko2000@gmail.com>
2021-09-14 00:38:37 +0300

    Init

---
+++ screen_ratio.cpp
@@ -0,0 +1,6 @@
+#include "screen_ratio.h"
+
+ScreenRatio::ScreenRatio(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ screen_ratio.h
@@ -0,0 +1,16 @@
+#ifndef SCREENRATIO_H
+#define SCREENRATIO_H
+
+#include <QObject>
+
+class ScreenRatio : public QObject
+{
+	Q_OBJECT
+public:
+	explicit ScreenRatio(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // SCREENRATIO_H
